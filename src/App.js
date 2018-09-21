e3573274ae4949937b9217cd8b70dd80bb158a30
aman-jung <amankarki87@gmail.com>
2018-09-21 15:53:14 +0530

    Intended Properly

--- src/App.js
+++ src/App.js
@@ -1,73 +1,65 @@
-import React,{Component} from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import Listbook from './Listbook';
-import {BrowserRouter,Route} from 'react-router-dom';
-import Search from './Search';
+import React, { Component } from "react";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Listbook from "./Listbook";
+import { BrowserRouter, Route } from "react-router-dom";
+import Search from "./Search";
 
 class BooksApp extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      books:[],
-      searchTerm:''
-    }
-  }
-  
-  componentDidMount(){
-      BooksAPI.getAll().then((books)=>{
-      this.setState(
-        {
-        books:books
-      })
-    }
-      )
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchTerm: ""
+    };
   }
 
-  moveShelf = (book,shelf)=>{
-    BooksAPI.update(book,shelf);
-    BooksAPI.getAll().then(books=>
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
       this.setState({
-        books:books
-      })
-      )
+        books: books
+      });
+    });
   }
+
+  moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+    BooksAPI.getAll().then(books =>
+      this.setState({
+        books: books
+      })
+    );
+  };
   // moveShelf =(book,shelf) => {
   //   BooksAPI.update(book,shelf);
-    
 
   // }
 
   render() {
-    const {books} = this.state;
+    const { books } = this.state;
     // console.log(book);
     return (
       <div className="app">
-           <BrowserRouter>
-           <div>
-             <Route 
-             exact 
-             path="/search" 
-             render={()=>( 
-             <Search moveShelf = {this.moveShelf}/>
-             )}/>
-             <Route 
-             exact 
-             path="/" 
-             render={
-               ()=>(
-                 <Listbook 
-                 books={books}
-                 moveShelf = {this.moveShelf}
-                 />
-               )
-              }
-            
-             />
-           </div>
-           </BrowserRouter>
+        <BrowserRouter>
+          <div>
+            <Route
+              exact
+              path="/search"
+              render={() => <Search moveShelf={this.moveShelf} />}
+            />
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <Listbook books={books} moveShelf={this.moveShelf} />
+              )}
+            />
+          </div>
+        </BrowserRouter>
         }
-      </div>  
-      ) } }
+      </div>
+    );
+  }
+}
 
-export default BooksApp
+export default BooksApp;
